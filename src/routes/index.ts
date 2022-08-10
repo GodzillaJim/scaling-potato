ef8757169df33dda2ec0aec9b60534205e4fadac
GodzillaJim <jimnanjoroge76@gmail.com>
2022-08-10 10:54:51 +0300

    v1:updated cors, remove procfile, health check

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,4 @@
-import { Router } from "express";
+import { Router, Response, Request } from "express";
 import authRoute from "./auth";
 import userRoute from "./user";
 import guestRoute from "./guest";
@@ -10,5 +10,15 @@ router.use("/auth", authRoute);
 router.use("/user", userRoute);
 router.use("/guest", guestRoute);
 router.use("/task", taskRouter);
+
+/**
+ * Handler root access, health check only
+ * @param _req
+ * @param res
+ */
+const handler = (_req: Request, res: Response) => {
+  res.json({ health: "Okay", message: "Hello world" });
+};
+router.get("/", handler);
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import express from "express";
 import helmet from "helmet";
 import morgan from "morgan";
-import { corsOptionsWhiteList, morganConfig } from "./config/config";
+import { morganConfig } from "./config/config";
 import router from "./routes";
 import socketServer from "./services/socket/server";
 
@@ -14,7 +14,7 @@ const app = express();
 const middleware = [
   morgan(morganConfig),
   helmet(),
-  cors(corsOptionsWhiteList),
+  cors(),
   express.json(),
   express.urlencoded({ extended: false }),
 ];
