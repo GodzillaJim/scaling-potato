a71da30f09c7be7ac483475e4b1ff9eacdd6b308
GodzillaJim <jimnanjoroge76@gmail.com>
2022-09-08 14:51:01 +0300

    v1:set mongo uri constant

--- src/config/mongodb.config.ts
+++ src/config/mongodb.config.ts
@@ -3,14 +3,17 @@ import mongoose from "mongoose";
 import logger from "./logger.config";
 
 export default class MongoDb {
+  mongoUri: string;
   constructor() {
+    this.mongoUri =
+      "mongodb+srv://collaborate_app:sherlockH0lmes05@cluster0.1cgtcoj.mongodb.net/?retryWrites=true&w=majority";
     config();
   }
 
   // eslint-disable-next-line class-methods-use-this
   connect() {
     mongoose
-      .connect(process.env.MONGO_URI)
+      .connect(this.mongoUri)
       .then(() => {
         logger.info("MongoDb connected");
       })
